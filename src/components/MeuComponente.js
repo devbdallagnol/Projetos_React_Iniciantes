af0e19405bc44bd1bd8a529ea8b008ca4a699878
Bruno DallAgnol <brunomorpheu@gmail.com>
2024-07-20 12:29:14 -0300

    updated - added MeuComponente.js, class div-cards css

---
+++ src/components/MeuComponente.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MeuComponente = () => {
+  return (
+    <div>
+        <h2>Meu Componente</h2>
+        <p>Este é um componente de JSX no ReactJS.</p>
+    </div>
+  )
+}
+
+export default MeuComponente
