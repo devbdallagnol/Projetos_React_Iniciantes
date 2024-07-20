5a5c9eb634c9123174ead285e998f6a64521f3df
Bruno DallAgnol <brunomorpheu@gmail.com>
2024-07-20 14:56:23 -0300

    updated - added Button.js, Card.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
-import './App.css';
-import MeuComponente from './components/MeuComponente';
-import Titulo from './components/Titulo';
-import ArquivoB from './components/imports_exports/ArquivoB';
-import ArquivoD from './components/imports_exports/ArquivoD';
-import ArquivoF from './components/imports_exports/ArquivoF';
-import ArquivoH from './components/imports_exports/ArquivoH';
+import "./App.css";
+import Card from "./components/Card";
+import MeuComponente from "./components/MeuComponente";
+import Titulo from "./components/Titulo";
+import ArquivoB from "./components/imports_exports/ArquivoB";
+import ArquivoD from "./components/imports_exports/ArquivoD";
+import ArquivoF from "./components/imports_exports/ArquivoF";
+import ArquivoH from "./components/imports_exports/ArquivoH";
 
 function App() {
   return (
     <div className="App">
       <h1>React para Iniciantes</h1>
       <hr />
-      <div className='div-cards'>
-        <div className='div-card'>
+      <div className="div-cards">
+        <div className="div-card">
           <MeuComponente />
         </div>
         <div className="div-card">
           <Titulo />
         </div>
-        <div className='div-card'>
+        <div className="div-card">
           <ArquivoB />
           <ArquivoD />
           <ArquivoF />
           <ArquivoH />
+        </div>
+        <div className="div-card">
+          <Card />
         </div>
       </div>
     </div>
---
+++ src/components/Card.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Button from "./Button";
+
+function Card() {
+    return (
+        <div>
+            <h3>Meu Cartão</h3>
+            <Button text="Clique aqui"/>
+        </div>
+    )
+}
+
+export default Card;
