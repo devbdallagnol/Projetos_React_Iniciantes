0e9ba3a2f5c75d210d3172b2faf926075344f1bb
Bruno DallAgnol <brunomorpheu@gmail.com>
2024-07-20 15:21:35 -0300

    updated - added module css .myComponent

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import MeuComponente from "./components/MeuComponente";
 import Titulo from "./components/Titulo";
 import EstilosInline from "./components/estilos/EstilosInline";
 import MyComponent from "./components/estilos/MyComponent";
+import MyComponent2 from "./components/estilos/MyComponent2";
 import ArquivoB from "./components/imports_exports/ArquivoB";
 import ArquivoD from "./components/imports_exports/ArquivoD";
 import ArquivoF from "./components/imports_exports/ArquivoF";
@@ -35,6 +36,9 @@ function App() {
         </div>
         <div className="div-card">
           <EstilosInline />
+        </div>
+        <div className="div-card">
+          <MyComponent2 />
         </div>
       </div>
     </div>
---
+++ src/components/estilos/MyComponent2.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./styles.module.css";
+
+const MyComponent2 = () => {
+  return <div className={styles.myComponent}>CONTEUDO DO COMPONENTE</div>;
+};
+
+export default MyComponent2;
