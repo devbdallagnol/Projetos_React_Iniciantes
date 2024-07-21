223244f871f5181de79df360a84d34f5639bedd8
Bruno DallAgnol <brunomorpheu@gmail.com>
2024-07-20 21:11:24 -0300

    updated - 3_LAYOUTS - added Footer.js

--- 3_LAYOUTS/layouts/src/App.js
+++ 3_LAYOUTS/layouts/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import CardBody from './components/CardBody';
+import Footer from './components/Footer';
 import NavBar from './components/NavBar';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <NavBar />
       <CardBody />
+      <Footer />
     </div>
   );
 }
