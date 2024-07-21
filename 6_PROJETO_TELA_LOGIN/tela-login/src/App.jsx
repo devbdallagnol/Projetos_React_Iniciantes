66f495b9a864c897f41956098e8fdb0d4911b3b7
Bruno DallAgnol <bruno.techdatascientist@gmail.com>
2024-07-21 17:38:09 -0300

    updated - 6_PROJETO_TELA_LOGIN - added Clock.jsx

--- 6_PROJETO_TELA_LOGIN/tela-login/src/App.jsx
+++ 6_PROJETO_TELA_LOGIN/tela-login/src/App.jsx
@@ -1,34 +1,14 @@
-import React, { useState } from "react";
-import "./Login.css";
-import "./App.css";
+import React from "react";
+import Login from "./components/Login";
+import Clock from "./components/Clock";
 
-function Login() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleLogin = () => {
-    // Logica de autenticação
-    alert(`Usuário: ${username}, Senha: ${password}`);
-  };
-
+const App = () => {
   return (
-    <div className='Login'>
-      <h2>Tela de Login</h2>
-      <input 
-        type="text"
-        placeholder="Usuário"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input 
-        type="password"
-        placeholder="Senha"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={handleLogin}>Login</button>
+    <div>
+      <Login />
+      <Clock />
     </div>
-  )
-}
+  );
+};
 
-export default Login;
+export default App;
---
+++ 6_PROJETO_TELA_LOGIN/tela-login/src/components/Login.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import "./Login.css";
+
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    // Logica de autenticação
+    alert(`Usuário: ${username}, Senha: ${password}`);
+  };
+
+  return (
+    <div className="Login">
+      <h2>Tela de Login</h2>
+      <input
+        type="text"
+        placeholder="Usuário"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Senha"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+}
+
+export default Login;
